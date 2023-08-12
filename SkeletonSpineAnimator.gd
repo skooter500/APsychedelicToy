adc0cbabec4e3d39954276e0e5bf285a1876119c
Bryan Duggan <skooter500@gmail.com>
2023-08-12 16:27:12 +0100

    bla

--- SkeletonSpineAnimator.gd
+++ SkeletonSpineAnimator.gd
@@ -1,33 +1,51 @@
-class_name SpineAnimator extends Node
+class_name SkeletonSpineAnimator extends Node
 
-export var bonePaths = []
+export var skeleton_path:NodePath
+var skeleton
 export var damping:float = 7
 export var angular_damping:float = 20
 				
 var bones = [] 
 var offsets = [] 
-var rotations = []
 
 func calculateOffsets():
+	skeleton = get_node(skeleton_path)
 	bones.clear()
 	offsets.clear()	
-	rotations.clear()
-	for i in bonePaths.size():
-		var bone = get_node(bonePaths[i])
+		
+	for i in skeleton.get_bone_count():
+		var bone = skeleton.get_bone_custom_pose(i)
+		
+		var txt = skeleton.get_bone_name(i) + str(bone.origin)  \
+				+ " Bone Pose: " + str(skeleton.get_bone_pose(i).origin) \
+				+ " Rest Pose: " + str(skeleton.get_bone_rest(i).origin) \
+				+ " Custom Pose: " + str(skeleton.get_bone_custom_pose(i).origin) \
+				+ " Global Pose: " + str(skeleton.get_bone_global_pose(i).origin) \
+				+ " Global Pose Rotation: " + str(skeleton.get_bone_global_pose(i).basis.get_euler()) \
+				
+		
+		print(txt)
 		bones.push_back(bone)
 		if i > 0:
-			var offset = bones[i].global_transform.origin - bones[i-1].global_transform.origin
-			offset = bones[i-1].global_transform.basis.xform_inv(offset)
+			var offset = bones[i].origin - bones[i-1].origin
+			offset = bones[i-1].basis.xform_inv(offset)
 			offsets.push_back(offset)
-			rotations.push_back(bones[i].transform.basis)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	calculateOffsets()
-
+	
+func _physics_process(delta):
+	var t = OS.get_system_time_msecs() / 500.0
+	for i in skeleton.get_bone_count():
+		var bone = skeleton.get_bone_global_pose(i)		
+		bone.origin
+		# bone.origin = pos
+		# skeleton.set_bone_pose(i, bone)
+		# skeleton.set_bone_pose_position(i, bone)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func __physics_process(delta):
 	for i in offsets.size():
 		var prev = bones[i]
 		var next = bones[i + 1]
@@ -45,7 +63,7 @@ func _physics_process(delta):
 		
 		# Why?
 		var target_rot = prev.global_transform.looking_at(next.global_transform.origin, prev.global_transform.basis.y).basis.orthonormalized()			
-		target_rot = target_rot * rotations[i]
+		# var next_rot = nextRot.slerp(prevRot, angular_damping * delta).orthonormalized()
 		 
 		next.global_transform.basis = next.global_transform.basis.slerp(target_rot, angular_damping * delta).orthonormalized()
 		
